4ef6ef98231348855194790d5db85fe922ce929f
patrick <dev@patrickflynn.co>
2020-03-05 09:06:44 -0500

    simplified opcode abstraction function

--- Opcode.hs
+++ Opcode.hs
@@ -17,12 +17,4 @@ toOpcode op
     | op == Jmp = 0xA3
     | otherwise = 0x00
     
-toChar op
-    | op == ILoad = (chr 0x20)
-    | op == IAdd = (chr 0x24)
-    | op == IPrint = (chr 0x29)
-    | op == IPop = (chr 0x31)
-    | op == Exit = (chr 0x10)
-    | op == Lbl = (chr 0x11)
-    | op == Jmp = (chr 0xA3)
-    | otherwise = (chr 0x00)
+toChar op = chr (toOpcode op)
